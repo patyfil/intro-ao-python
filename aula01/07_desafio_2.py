6bb75e578898e137073822cf97e7332bab2bc5cf
patyfil <patyfil@gmail.com>
2022-04-20 14:35:57 -0300

    desafio 01 e 02 feito

--- aula01/07_desafio_2.py
+++ aula01/07_desafio_2.py
@@ -1,8 +1,14 @@
 # 1. Pergunte ao usuário em qual cidade e o estado em que ele(a) mora
-
+nome = input('Qual é o seu nome?')
+cidade = input('Qual é a sua cidade que você mora?')
+estado = input('Qual é o Estado que você mora?')
 # 2. Imprima uma mensagem diga a cidade em que o usuário mora. 
+print(nome, "é da cidade de", cidade, estado)
 #    O nome da cidade deve estar todo em letras maiúsculas.
-
+print(nome, "é da cidade de", cidade.upper(), estado)
 # 3. Imprima uma mensagem diga o estado em que o usuário mora. 
+print(nome, "é do Estado de:", estado)
 #    O nome do estado deve estar todo em letras maiúsculas.
+print(nome, "é do Estado de:", estado.upper())
 #    Utilize uma forma de formatação de string diferente da que foi usada no item 2.
+print(f'{nome.title()} é da cidade de {cidade.capitalize()}/{estado.upper()}')
