812d8a2b055cfb4d8975786d1aa1f3c6f03394ae
patyfil <patyfil@gmail.com>
2022-05-02 17:17:27 -0300

    add cabeçalho no desafio 5

--- aula03/03_desafio_5.py
+++ aula03/03_desafio_5.py
@@ -1,3 +1,4 @@
+# Desafio 5 - Listas e loops
 # Crie uma lista com 60 números começando com o valor 1 e indo até o número 60 (ou seja, o número 60 deve estar na lista).
 lista = list(range(1, 61))
 print(lista)
