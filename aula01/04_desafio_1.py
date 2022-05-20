abeb584d6fc64ae80727ef3b557b2a8364873fea
patyfil <patyfil@gmail.com>
2022-05-20 18:19:40 -0300

    melhorando o código

--- aula01/04_desafio_1.py
+++ aula01/04_desafio_1.py
@@ -1,13 +1,16 @@
 # Desafio 1 - print e input
 # 1. Peça ao seu usuário para inserir 3 dados: nome, idade e cidade.
 
-nome = input('Qual é o seu nome?')
-idade = input('Qual é a sua idade?')
-cidade = input('Qual é a sua cidade?')
-print("Meu nome é: " + nome)
-print("Minha idade é: " + idade)
-print("Minha cidade é: " + cidade)
-
+nome = str(input('Qual é o seu nome?'))
+idade = int(input('Qual é a sua idade?'))
+cidade = str(input('Qual é a sua cidade?'))
+print('='*40)
+print("O nome digitado foi: " + nome)
+print(f"A idade digitada foi: ", idade)
+print("A cidade digitada foi: " + cidade)
+print('='*40)
 # 2. Imprima no console uma única mensagem desejando boas vindas ao usuário.
-print("Seja Bem vindo(a) " + nome)
+print('\n')
+print("Seja Bem vindo(a) " + nome + '!')
 # print(f'Oi {nome}! Você tem {idade} anos de idade e mora em {cidade}')
+print('\n')
