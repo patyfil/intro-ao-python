c91d4dbc5ba4066224681d2b614c9c308ee97efb
patyfil <patyfil@gmail.com>
2022-05-20 20:59:30 -0300

    melhorando o código

--- aula04/03_desafio_7.py
+++ aula04/03_desafio_7.py
@@ -9,24 +9,30 @@ def calcular_media(valores):
         soma += valor  # soma = soma + valor
         tamanho = i + 1
         media = soma / tamanho
-        return media
+    return media
 
 
 continuar = True
 valores = []
 while continuar:
-    valor = input('Digite um número para entrar na sua média ou "ok" para calcular o valor:')
+    valor = input('Digite um número para entrar na sua média ou "ok" para calcular o valor: ')
     if valor.isdigit():  # SE a var 'valor' receber somente número, a var 'continuar' será TRUE / Retorna TRUE se todos os caracteres forem dígitos, caso contrário, False.
         valores.append(int(valor)) # add valor digitado no final da sequencia valores[]
         continuar = True        
     elif valor.lower() == 'ok': # SENÃO SE a var 'valor' receber 'ok', a var 'continuar' será FALSE
         continuar = False
     else:
+        print('='*60)
+        print('\n')
         print('Valor digitado é inválido, digite novamente')
+        print('\n')
+        print('='*60)
         continuar = True
 
 media = calcular_media(valores)
-print('A média calculada para os valores {} foi de {}'.format(valores, media))
-  
-  
+print('\n')
+print('='*60)
+print('A média calculada para os valores: {} foi de {:.2f}'.format(valores, media))
+print('='*60)
+print('\n')
   
