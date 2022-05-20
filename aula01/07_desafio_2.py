d8e7c627204e2b0f7edf9d890a896ebd5e4b5023
patyfil <patyfil@gmail.com>
2022-05-20 18:36:31 -0300

    melhorando o código

--- aula01/07_desafio_2.py
+++ aula01/07_desafio_2.py
@@ -1,15 +1,16 @@
 # Desafio 2 - Strings
 # 1. Pergunte ao usuário em qual cidade e o estado em que ele(a) mora
-nome = input('Qual é o seu nome?')
-cidade = input('Qual é a sua cidade que você mora?')
-estado = input('Qual é o Estado que você mora?')
+nome = input('Qual é o seu nome? ')
+cidade = str(input('Qual é a sua cidade que você mora? '))
+estado = str(input('Qual é o Estado que você mora? '))
+print('='*40)
+print('\n')
 # 2. Imprima uma mensagem diga a cidade em que o usuário mora. 
-print(nome, "é da cidade de", cidade, estado)
 #    O nome da cidade deve estar todo em letras maiúsculas.
-print(nome, "é da cidade de", cidade.upper(), estado)
+print(nome.upper(), "é da Cidade de", cidade.upper(),'/', estado.upper())
 # 3. Imprima uma mensagem diga o estado em que o usuário mora. 
-print(nome, "é do Estado de:", estado)
 #    O nome do estado deve estar todo em letras maiúsculas.
-print(nome, "é do Estado de:", estado.upper())
 #    Utilize uma forma de formatação de string diferente da que foi usada no item 2.
-print(f'{nome.title()} é da cidade de {cidade.capitalize()}/{estado.upper()}')
+print('\n')
+print(nome.upper() + ' ' + 'é do Estado de ' + estado.upper())
+print('\n')
